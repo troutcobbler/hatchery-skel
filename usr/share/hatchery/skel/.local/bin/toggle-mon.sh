74df6a11b877e9d97f49f6d43f121a3d8d3deb29
Will Elliott <troutcobbler@gmail.com>
2023-08-30 00:22:15 -0600

    updated the toggle-mon.sh script and the hyprland.conf

--- usr/share/hatchery/skel/.local/bin/toggle-mon.sh
+++ usr/share/hatchery/skel/.local/bin/toggle-mon.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 intern=eDP-1
 extern=HDMI-2
+extern_hyprland=HDMI-A-2
 
 if xrandr | grep "$extern disconnected"; then
     xrandr --output "$extern" --off --output "$intern" --auto
 else
     xrandr --output "$intern" --off --output "$extern" --auto
+fi
+
+if wlr-randr | grep "$extern_hyprland"; then
+    wlr-randr --output $intern --off && hyprctl dispatch workspace 1
 fi
